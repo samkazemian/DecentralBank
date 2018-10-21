4b98acccb746cd6df2b9b650ccb8b08b36312a50
Chris Atanasian <catanasian@gmail.com>
2018-10-20 20:28:58 -0700

    Minor adjustments

--- helpers.cpp
+++ helpers.cpp
@@ -1,5 +1,7 @@
 #include "helpers.hpp"
 
+// to use this file in a contract, include this file in the .cpp file of the contract
+
 bool isLowerThanPeggedPrice(const float currentPrice) {
   return currentPrice < 1.0;
 }
--- helpers.hpp
+++ helpers.hpp
@@ -1,8 +1,6 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-#include <eosiolib/print.hpp>
-
 bool isLowerThanPeggedPrice(const float currentPrice);
 
 #endif
