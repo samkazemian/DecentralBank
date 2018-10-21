15505e99322641ce777d907697a1e93b77e8220c
Chris Atanasian <catanasian@gmail.com>
2018-10-20 18:28:51 -0700

    Add basic helpers files

---
+++ helpers.cpp
@@ -0,0 +1,5 @@
+#include "helpers.hpp"
+
+bool isLowerThanPeggedPrice(const float currentPrice) {
+  return currentPrice < 1.0;
+}
---
+++ helpers.hpp
@@ -0,0 +1,8 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <eosiolib/print.hpp>
+
+bool isLowerThanPeggedPrice(const float currentPrice);
+
+#endif
