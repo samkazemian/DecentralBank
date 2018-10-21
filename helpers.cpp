fd1e5956cb694277c486a7c05bcf6d2909100443
Chris Atanasian <catanasian@gmail.com>
2018-10-20 22:55:13 -0700

    Remove unnecessary const

--- helpers.cpp
+++ helpers.cpp
@@ -2,6 +2,6 @@
 
 // to use this file in a contract, include this file in the .cpp file of the contract
 
-bool isLowerThanPeggedPrice(const float currentPrice) {
+bool isLowerThanPeggedPrice(float currentPrice) {
   return currentPrice < 1.0;
 }
